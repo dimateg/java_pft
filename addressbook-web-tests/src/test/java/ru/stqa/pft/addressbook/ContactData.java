d5431729833d34f162b01adb118c885f26409a03
dimateg <dimateg@gmail.com>
2023-06-14 21:07:05 +0300

    Задание №4: Сделать тесты для создания групп и контактов в адресной книге

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String lastname;
+    private final String nickname;
+    private final String home;
+    private final String mobile;
+    private final String email;
+
+    public ContactData(String firstname, String lastname, String nickname, String home, String mobile, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.home = home;
+        this.mobile = mobile;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
