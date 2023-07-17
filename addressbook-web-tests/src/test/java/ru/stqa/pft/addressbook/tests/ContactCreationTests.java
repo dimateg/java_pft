542b01bf6df41b3aea3a2e9d8f683a1987a4d078
dimateg <dimateg@gmail.com>
2023-07-17 14:52:29 +0300

    Лекция 5.1. Как правильно запускать браузер

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class ContactCreationTests extends TestBase {
 
 
-    @Test
+    @Test(enabled = false)
     public void testContactCreationTests() throws Exception {
         List<ContactData> before = app.getContactHelper().getContactList();
         app.getContactHelper().gotoAddContactPage();
